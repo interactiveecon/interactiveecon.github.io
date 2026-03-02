3844d7ad89da71a1ac34b46c3d335597408b0b2b
interactiveecon <jeff.allen19@icloud.com>
2026-03-02 15:38:23 -0800

    Create data.js

---
+++ classes/econ002/money/fractional-reserve-banking/data.js
@@ -0,0 +1,9 @@
+// data.js
+window.MONEY_CREATION = {
+  banks: ["Bank A", "Bank B", "Bank C", "Bank D", "Bank E", "Bank F"],
+  rrRange: [0.08, 0.20],
+  injectionRange: [20, 80],      // ΔR
+  maxSteps: 10,
+  minLoanToContinue: 0.5,        // stop when next loan would be tiny
+  excessReserveRate: 0.05        // if enabled: extra reserves = 5% of new deposits
+};
