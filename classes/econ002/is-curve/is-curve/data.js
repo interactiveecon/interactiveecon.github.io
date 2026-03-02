b9c7820b2edd27179a2419339b6d0a7d316b5824
interactiveecon <jeff.allen19@icloud.com>
2026-03-02 11:22:41 -0800

    Update data.js

--- classes/econ002/is-curve/is-curve/data.js
+++ classes/econ002/is-curve/is-curve/data.js
@@ -1,30 +1,17 @@
 // data.js
-// Baseline parameters for KC/IS lab.
-
 window.KCIS_DATA = {
-  // Consumption and investment parameters
   params: {
     C0: 120,
     MPC: 0.6,
     I0: 140,
-    b: 12,     // sensitivity of investment to r: I = I0 - b*r
+    b: 12
   },
-
-  // Baseline policy settings (sliders start here)
-  baseline: {
-    r: 4.0,
-    G: 150,
-    T: 150
-  },
-
-  // Slider ranges (must match index.html)
+  baseline: { r: 4.0, G: 150, T: 150 },
   ranges: {
     r: { min: 0, max: 10, step: 0.1 },
     G: { min: 50, max: 250, step: 5 },
     T: { min: 50, max: 250, step: 5 }
   },
-
-  // Scenario shock sizes
   shocks: {
     r: [ +1.0, -1.0, +1.5, -1.5 ],
     G: [ +20, -20, +30, -30 ],
